8f8b360fc76495b57ad053372ae868d6dbc45a57
brunofsabino <brunoferrazsabino@gmail.com>
2023-03-05 21:17:13 -0300

    update public

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,36 @@
-const simpleCarousel = document.querySelector(".js-carousel--simple");
- 
-new Glider(simpleCarousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  dots: ".js-carousel--simple-dots",
-  arrows: {
-    prev: ".js-carousel--simple-prev",
-    next: ".js-carousel--simple-next",
-  },
-});
+let totalSlides = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
+
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
+document.querySelector('.slider--width').style.width = 
+    `${sliderWidth * totalSlides}px`;
+
+document.querySelector('.slider--controls').style.width = 
+    `${sliderWidth}px`;
+document.querySelector('.slider--controls').style.height = 
+    `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+}
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides-1)) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = 
+        `-${newMargin}px`;
+}
+
+setInterval(goNext, 5000);
